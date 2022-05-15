f733edf455dfce92436280ee34ddabd7a496b329
Martin Morães <martin.moraes@outlook.com>
2022-05-15 10:20:56 -0300

    refact alter message verify token

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -4,27 +4,29 @@ module.exports = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
     // req.decodedToken = 'noToken';
-    return loggerEndReturn(res, 'Token not informed.');
+    return loggerEndReturn(res);
   }
 
   const parts = authHeader.split(' ');
-  if (parts.length !== 2) return loggerEndReturn(res, 'Token error.');
+  if (parts.length !== 2) return loggerEndReturn(res);
 
   const [scheme, token] = parts;
-  if (!/^Bearer$/i.test(scheme))
-    return loggerEndReturn(res, 'Token unformatted.');
+  if (!/^Bearer$/i.test(scheme)) return loggerEndReturn(res);
 
   try {
     const decoded = await new Token().verify(token);
     req.decodedToken = decoded;
     return next();
   } catch (error) {
-    return loggerEndReturn(res, 'Failed to authenticate token.');
+    return loggerEndReturn(res);
   }
 };
 
-const loggerEndReturn = (res, message) => {
-  const mensage = JSON.stringify({ token: false, message: message });
+const loggerEndReturn = res => {
+  const mensage = JSON.stringify({
+    token: false,
+    message: 'Access token is missing or invalid',
+  });
   console.error('auth', mensage);
   res.status(401).send(mensage);
   return;
